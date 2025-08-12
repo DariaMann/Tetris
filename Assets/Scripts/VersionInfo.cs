030aec9c31fca21b2babeff14bc18d232fe6217e
ed <dariamandev@gmail.com>
2025-08-12 19:59:23 +0300

    Везде: обновила Appodeal; поправила баг с неуспешной сборкой билда под Android

--- Assets/Scripts/VersionInfo.cs
+++ Assets/Scripts/VersionInfo.cs
@@ -1,6 +1,6 @@
 // Auto-generated during build
 public static class VersionInfo
 {
-    public const string Version = "1.1";
+    public const string Version = "1.2";
     public const string Build = "2";
 }
