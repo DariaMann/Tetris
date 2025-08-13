ee797c8cb3fab2fe76d8fca77dd1ca1b39bb2173
dmanukyants <dmanukyants@gear-games.com>
2025-08-13 14:39:40 +0300

    Везде: обновила библиотеку платежей андроид

--- Assets/Scripts/VersionInfo.cs
+++ Assets/Scripts/VersionInfo.cs
@@ -1,6 +1,6 @@
 // Auto-generated during build
 public static class VersionInfo
 {
-    public const string Version = "1.2";
-    public const string Build = "2";
+    public const string Version = "1.3";
+    public const string Build = "3";
 }
